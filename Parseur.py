0c3c1af0c3cc4c2ca1eca97b60cf9cd987ccad58
Matis Herrmann <matis_herrmann@hotmail.fr>
2021-12-06 20:59:41 +0100

    Factorised Similar Code and Fix issue when Line ended with a space

--- Parseur.py
+++ Parseur.py
@@ -1,12 +1,13 @@
 import os.path
 import sys
 
-file_path = "code_asm/test_integration/data_processing/1-4_instructions.s"
+file_path = "code_asm/test_integration/data_processing/11-12_instructions.s"
 new_file_name = "Parsed_File.bin"
 
 
 # Converts an Integer to a Binary String
 def int_2_bin(n, size=0):
+    n = str(n).replace("#", "").replace("r", "")
     binary_text = "{0:b}".format(int(n))
     if size != 0 and len(binary_text) > size:
         binary_text = binary_text[len(binary_text) - size:]
@@ -19,11 +20,12 @@ def bin_2_hex(bin_string):
 
 
 if __name__ == '__main__':
-    # Initialise list of Instructions
+    # Initialise list of Instructions and Count
     instructions = []
+    count_instructions = 0
 
     # Possibility to give Path to any File
-    string_entered = str(input("Enter Path of wanted file to Parse:"))
+    string_entered = str(input("Enter Path of wanted file to Parse: "))
     if string_entered:
         file_path = string_entered
     else:
@@ -35,126 +37,142 @@ if __name__ == '__main__':
     file = open(file_path, 'r')
     for line in file.readlines():
         line = line.replace("\n", "").replace(",", "").lower()
+        if line[-1:] == " ":
+            line = line[:-1]
 
         # Only Reads lines that are not empty and not in comments
         if line[:1] != "@" and line:
-            sys.stdout.write(line + " ")
+            sys.stdout.write(line)
+            binary = ""
             words = line.split(" ")
             words[0] = words[0][:3]
-            binary_string = ""
-
-            for i in range(len(words)):
-                if i != 0:
-                    words[i] = words[i].replace("#", "").replace("r", "")
 
             # Matches Keyword with Instructions
             match words[0]:
                 case "lsl":
                     if len(words) == 4:
-                        binary_string = "00000" + int_2_bin(words[3], 5) + int_2_bin(words[2], 3) + int_2_bin(words[1],
-                                                                                                              3)
+                        binary = "00000" + int_2_bin(words[3], 5) + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
                     if len(words) == 3:
-                        binary_string = "0100000010" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+                        binary = "0100000010" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+
                 case "lsr":
                     if len(words) == 4:
-                        binary_string = "00001" + int_2_bin(words[3], 5) + int_2_bin(words[2], 3) + int_2_bin(words[1],
-                                                                                                              3)
+                        binary = "00001" + int_2_bin(words[3], 5) + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
                     if len(words) == 3:
-                        binary_string = "0100000011" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+                        binary = "0100000011" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+
                 case "asr":
                     if len(words) == 4:
-                        binary_string = "00010" + int_2_bin(words[3], 5) + int_2_bin(words[2], 3) + int_2_bin(words[1],
-                                                                                                              3)
+                        binary = "00010" + int_2_bin(words[3], 5) + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
                     if len(words) == 3:
-                        binary_string = "0100000100" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+                        binary = "0100000100" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+
                 case "add":
-                    if len(words) == 4:  # words[3] as a Register
-                        binary_string = "0001100" + int_2_bin(words[3], 3) + int_2_bin(words[2], 3) + int_2_bin(
-                            words[1], 3)
-                    if len(words) == 4:  # words[3] as a #Constant
-                        binary_string = "0001110" + int_2_bin(words[3], 3) + int_2_bin(words[2], 3) + int_2_bin(
-                            words[1], 3)
+                    if len(words) == 4 and words[3][:1] == "r":
+                        binary = "0001100" + int_2_bin(words[3], 3) + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+                    if len(words) == 4 and words[3][:1] == "#":
+                        binary = "0001110" + int_2_bin(words[3], 3) + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
                     if len(words) == 3:  # Number of Arguments ? Can change ?
-                        binary_string = "00110" + int_2_bin(words[1], 3) + int_2_bin(words[2], 8)
-                case "sub":  # words[3] as a Register
-                    if len(words) == 4:
-                        binary_string = "0001101" + int_2_bin(words[3], 3) + int_2_bin(words[2], 3) + int_2_bin(
-                            words[1], 3)
-                    if len(words) == 4:  # words[3] as a #Constant
-                        binary_string = "0001111" + int_2_bin(words[3], 3) + int_2_bin(words[2], 3) + int_2_bin(
-                            words[1], 3)
+                        binary = "00110" + int_2_bin(words[1], 3) + int_2_bin(words[2], 8)
+
+                case "sub":
+                    if len(words) == 4 and words[3][:1] == "r":
+                        binary = "0001101" + int_2_bin(words[3], 3) + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+                    if len(words) == 4 and words[3][:1] == "#":
+                        binary = "0001111" + int_2_bin(words[3], 3) + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
                     if len(words) == 3:  # Number of Arguments ? Can Change ?
-                        binary_string = "00111" + int_2_bin(words[1], 3) + int_2_bin(words[2], 8)
+                        binary = "00111" + int_2_bin(words[1], 3) + int_2_bin(words[2], 8)
+
                 case "mov":
                     if len(words) == 3:
-                        binary_string = "00100" + int_2_bin(words[1], 3) + int_2_bin(words[2], 8)
+                        binary = "00100" + int_2_bin(words[1], 3) + int_2_bin(words[2], 8)
+
                 case "cmp":
-                    if len(words) == 3:  # words[2] as a #Constant
-                        binary_string = "00101" + int_2_bin(words[1], 3) + int_2_bin(words[2], 8)
-                    if len(words) == 3:  # words[2] as a Register
-                        binary_string = "0100001010" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+                    if len(words) == 3 and words[2][:1] == "#":
+                        binary = "00101" + int_2_bin(words[1], 3) + int_2_bin(words[2], 8)
+                    if len(words) == 3 and words[2][:1] == "r":
+                        binary = "0100001010" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+
                 case "and":
                     if len(words) == 3:
-                        binary_string = "0100000000" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+                        binary = "0100000000" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+
                 case "eor":
                     if len(words) == 3:
-                        binary_string = "0100000001" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+                        binary = "0100000001" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+
                 case "adc":
                     if len(words) == 3:
-                        binary_string = "0100000101" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+                        binary = "0100000101" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+
                 case "sbc":
                     if len(words) == 3:
-                        binary_string = "0100000110" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+                        binary = "0100000110" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+
                 case "ror":
                     if len(words) == 3:
-                        binary_string = "0100000111" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+                        binary = "0100000111" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+
                 case "tst":
                     if len(words) == 3:
-                        binary_string = "0100001000" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+                        binary = "0100001000" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+
                 case "rsb":
                     if len(words) == 4:
-                        binary_string = "0100001001" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+                        binary = "0100001001" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+
                 case "cmn":
                     if len(words) == 3:
-                        binary_string = "0100001011" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+                        binary = "0100001011" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+
                 case "orr":
                     if len(words) == 3:
-                        binary_string = "0100001100" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+                        binary = "0100001100" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+
                 case "mul":
                     if len(words) == 4:
-                        binary_string = "0100001101" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+                        binary = "0100001101" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+
                 case "bic":
                     if len(words) == 3:
-                        binary_string = "0100001110" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+                        binary = "0100001110" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+
                 case "mvn":
                     if len(words) == 3:
-                        binary_string = "0100001111" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+                        binary = "0100001111" + int_2_bin(words[2], 3) + int_2_bin(words[1], 3)
+
                 case "str":
                     if len(words) == 3:
-                        binary_string = "10010" + int_2_bin(words[1], 3) + int_2_bin(int(int(words[2]) / 4), 8)
+                        binary = "10010" + int_2_bin(words[1], 3) + int_2_bin(int(int(words[2]) / 4), 8)
+
                 case "ldr":
                     if len(words) == 3:
-                        binary_string = "10011" + int_2_bin(words[1], 3) + int_2_bin(int(int(words[2]) / 4), 8)
+                        binary = "10011" + int_2_bin(words[1], 3) + int_2_bin(int(int(words[2]) / 4), 8)
+
                 case "add":  # With SP
                     if len(words) == 3:  # Number of Arguments ?
-                        binary_string = "101100000" + int_2_bin(int(int(words[2]) / 4), 7)
+                        binary = "101100000" + int_2_bin(int(int(words[2]) / 4), 7)
+
                 case "sub":  # With SP
                     if len(words) == 3:  # Number of Arguments ?
-                        binary_string = "101100001" + int_2_bin(int(int(words[2]) / 4), 7)
+                        binary = "101100001" + int_2_bin(int(int(words[2]) / 4), 7)
+
                 case "B":  # Branches Conditional and Unconditional
                     if len(words) == 3:  # words[1] & words[2] ?
-                        binary_string = "1101" + int_2_bin(words[2], 4) + int_2_bin(words[1], 8)
+                        binary = "1101" + int_2_bin(words[2], 4) + int_2_bin(words[1], 8)
                     elif len(words) == 2:
-                        binary_string = "11100" + int_2_bin(words[1], 11)  # words[1] ?
+                        binary = "11100" + int_2_bin(words[1], 11)  # words[1] ?
+
                 case _:
                     sys.stdout.write("Nothing ")
 
             # Adding Binary string to Instructions
-            if binary_string:
-                instructions.append(binary_string)
+            if binary:
+                instructions.append(binary)
+            count_instructions += 1
             print()
-    print()
     file.close()
+    print()
 
     # Convert all Binaries to Hexadecimals
     for i in range(len(instructions)):
@@ -171,4 +189,4 @@ if __name__ == '__main__':
     file.close()
 
     print("--- Instructions Parsed ---")
-    print(instructions)
+    print(instructions, str(len(instructions)) + "/" + str(count_instructions), "Instructions")
