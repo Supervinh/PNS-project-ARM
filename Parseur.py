869690ca4215844762bc0572892ba9d54f9f3a52
Matis Herrmann <matis_herrmann@hotmail.fr>
2021-12-09 15:31:51 +0100

    Added Arguments to Table

--- Parseur.py
+++ Parseur.py
@@ -1,6 +1,7 @@
 import os.path
+import re
 import sys
-import webbrowser
+# import webbrowser
 from tkinter import filedialog, Tk
 
 import prettytable
@@ -22,137 +23,176 @@ def bin_2_hex(bin_string):
     return hex(int(bin_string, 2))
 
 
+# Only Keeps Integers in a String
+def onlyInts(text):
+    return re.sub("[^0-9]", "", text)
+
+
 # Matches Keyword with Instructions
 def parse_from_line(w):
     binary = ""
+    args = ""
     match w[0]:
         case "lsl":
             if len(w) == 4:
+                args = "Rd=" + onlyInts(w[1]) + ", Rm=" + onlyInts(w[2]) + ", imm5=" + onlyInts(w[3])
                 binary = "00000" + int_2_bin(w[3], 5) + int_2_bin(w[2], 3) + int_2_bin(w[1], 3)
             if len(w) == 3:
+                args = "Rdn=" + onlyInts(w[1]) + ", Rm=" + onlyInts(w[2])
                 binary = "0100000010" + int_2_bin(w[2], 3) + int_2_bin(w[1], 3)
 
         case "lsr":
             if len(w) == 4:
+                args = "Rd=" + onlyInts(w[1]) + ", Rm=" + onlyInts(w[2]) + ", imm5=" + onlyInts(w[3])
                 binary = "00001" + int_2_bin(w[3], 5) + int_2_bin(w[2], 3) + int_2_bin(w[1], 3)
             if len(w) == 3:
+                args = "Rdn=" + onlyInts(w[1]) + ", Rm=" + onlyInts(w[2])
                 binary = "0100000011" + int_2_bin(w[2], 3) + int_2_bin(w[1], 3)
 
         case "asr":
             if len(w) == 4:
+                args = "Rd=" + onlyInts(w[1]) + ", Rm=" + onlyInts(w[2]) + ", imm5=" + onlyInts(w[3])
                 binary = "00010" + int_2_bin(w[3], 5) + int_2_bin(w[2], 3) + int_2_bin(w[1], 3)
             if len(w) == 3:
+                args = "Rdn=" + onlyInts(w[1]) + ", Rm=" + onlyInts(w[2])
                 binary = "0100000100" + int_2_bin(w[2], 3) + int_2_bin(w[1], 3)
 
         case "add":
             if len(w) == 4 and w[3][:1] == "r":
+                args = "Rd=" + onlyInts(w[1]) + ", Rn=" + onlyInts(w[2]) + ", Rm=" + onlyInts(w[3])
                 binary = "0001100" + int_2_bin(w[3], 3) + int_2_bin(w[2], 3) + int_2_bin(w[1], 3)
             if len(w) == 4 and w[3][:1] == "#":
+                args = "Rd=" + onlyInts(w[1]) + ", Rn=" + onlyInts(w[2]) + ", imm3=" + onlyInts(w[3])
                 binary = "0001110" + int_2_bin(w[3], 3) + int_2_bin(w[2], 3) + int_2_bin(w[1], 3)
             if len(w) == 3:  # Number of Arguments ? Can change ?
+                args = "Rdn=" + onlyInts(w[1]) + ", imm8=" + onlyInts(w[2])
                 binary = "00110" + int_2_bin(w[1], 3) + int_2_bin(w[2], 8)
 
         case "sub":
             if len(w) == 4 and w[3][:1] == "r":
+                args = "Rd=" + onlyInts(w[1]) + ", Rn=" + onlyInts(w[2]) + ", Rm=" + onlyInts(w[3])
                 binary = "0001101" + int_2_bin(w[3], 3) + int_2_bin(w[2], 3) + int_2_bin(w[1], 3)
             if len(w) == 4 and w[3][:1] == "#":
+                args = "Rd=" + onlyInts(w[1]) + ", Rn=" + onlyInts(w[2]) + ", imm3=" + onlyInts(w[3])
                 binary = "0001111" + int_2_bin(w[3], 3) + int_2_bin(w[2], 3) + int_2_bin(w[1], 3)
             if len(w) == 3:  # Number of Arguments ? Can Change ?
+                args = "Rdn=" + onlyInts(w[1]) + ", imm8=" + onlyInts(w[2])
                 binary = "00111" + int_2_bin(w[1], 3) + int_2_bin(w[2], 8)
 
         case "mov":
             if len(w) == 3:
+                args = "Rd=" + onlyInts(w[1]) + ", imm8=" + onlyInts(w[2])
                 binary = "00100" + int_2_bin(w[1], 3) + int_2_bin(w[2], 8)
 
         case "cmp":
             if len(w) == 3 and w[2][:1] == "#":
+                args = "Rd=" + onlyInts(w[1]) + ", imm8=" + onlyInts(w[2])
                 binary = "00101" + int_2_bin(w[1], 3) + int_2_bin(w[2], 8)
             if len(w) == 3 and w[2][:1] == "r":
+                args = "Rn=" + onlyInts(w[1]) + ", Rm=" + onlyInts(w[2])
                 binary = "0100001010" + int_2_bin(w[2], 3) + int_2_bin(w[1], 3)
 
         case "and":
             if len(w) == 3:
+                args = "Rdn=" + onlyInts(w[1]) + ", Rm=" + onlyInts(w[2])
                 binary = "0100000000" + int_2_bin(w[2], 3) + int_2_bin(w[1], 3)
 
         case "eor":
             if len(w) == 3:
+                args = "Rdn=" + onlyInts(w[1]) + ", Rm=" + onlyInts(w[2])
                 binary = "0100000001" + int_2_bin(w[2], 3) + int_2_bin(w[1], 3)
 
         case "adc":
             if len(w) == 3:
+                args = "Rdn=" + onlyInts(w[1]) + ", Rm=" + onlyInts(w[2])
                 binary = "0100000101" + int_2_bin(w[2], 3) + int_2_bin(w[1], 3)
 
         case "sbc":
             if len(w) == 3:
+                args = "Rdn=" + onlyInts(w[1]) + ", Rm=" + onlyInts(w[2])
                 binary = "0100000110" + int_2_bin(w[2], 3) + int_2_bin(w[1], 3)
 
         case "ror":
             if len(w) == 3:
+                args = "Rdn=" + onlyInts(w[1]) + ", Rm=" + onlyInts(w[2])
                 binary = "0100000111" + int_2_bin(w[2], 3) + int_2_bin(w[1], 3)
 
         case "tst":
             if len(w) == 3:
+                args = "Rn=" + onlyInts(w[1]) + ", Rm=" + onlyInts(w[2])
                 binary = "0100001000" + int_2_bin(w[2], 3) + int_2_bin(w[1], 3)
 
         case "rsb":
             if len(w) == 4:
+                args = "Rd=" + onlyInts(w[1]) + ", Rn=" + onlyInts(w[2])
                 binary = "0100001001" + int_2_bin(w[2], 3) + int_2_bin(w[1], 3)
 
         case "cmn":
             if len(w) == 3:
+                args = "Rdn=" + onlyInts(w[1]) + ", Rm=" + onlyInts(w[2])
                 binary = "0100001011" + int_2_bin(w[2], 3) + int_2_bin(w[1], 3)
 
         case "orr":
             if len(w) == 3:
+                args = "Rdm=" + onlyInts(w[1]) + ", Rm=" + onlyInts(w[2])
                 binary = "0100001100" + int_2_bin(w[2], 3) + int_2_bin(w[1], 3)
 
         case "mul":
             if len(w) == 4:
+                args = "Rdn=" + onlyInts(w[1]) + ", Rm=" + onlyInts(w[2]) + ", Rdm=" + onlyInts(w[3])
                 binary = "0100001101" + int_2_bin(w[2], 3) + int_2_bin(w[1], 3)
 
         case "bic":
             if len(w) == 3:
+                args = "Rdn=" + onlyInts(w[1]) + ", Rm=" + onlyInts(w[2])
                 binary = "0100001110" + int_2_bin(w[2], 3) + int_2_bin(w[1], 3)
 
         case "mvn":
             if len(w) == 3:
+                args = "Rd=" + onlyInts(w[1]) + ", Rm=" + onlyInts(w[2])
                 binary = "0100001111" + int_2_bin(w[2], 3) + int_2_bin(w[1], 3)
 
         case "str":
             if len(w) == 3:
+                args = "Rt=" + onlyInts(w[1]) + ", imm8=" + onlyInts(w[2])
                 binary = "10010" + int_2_bin(w[1], 3) + int_2_bin(int(int(w[2]) / 4), 8)
 
         case "ldr":
             if len(w) == 3:
+                args = "Rt=" + onlyInts(w[1]) + ", imm8=" + onlyInts(w[2])
                 binary = "10011" + int_2_bin(w[1], 3) + int_2_bin(int(int(w[2]) / 4), 8)
 
         case "add":  # With SP
-            if len(w) == 3:  # Number of Arguments ?
-                binary = "101100000" + int_2_bin(int(int(w[2]) / 4), 7)
+            if len(w) == 2:  # Number of Arguments ?
+                args = "imm7=" + onlyInts(w[1])
+                binary = "101100000" + int_2_bin(int(int(w[1]) / 4), 7)
 
         case "sub":  # With SP
-            if len(w) == 3:  # Number of Arguments ?
-                binary = "101100001" + int_2_bin(int(int(w[2]) / 4), 7)
+            if len(w) == 2:  # Number of Arguments ?
+                args = "imm7=" + onlyInts(w[1])
+                binary = "101100001" + int_2_bin(int(int(w[1]) / 4), 7)
 
         case "B":  # Branches Conditional and Unconditional
             if len(w) == 3:  # words[1] & words[2] ?
+                args = "cond=" + onlyInts(w[1]) + ", imm8=" + onlyInts(w[2])
                 binary = "1101" + int_2_bin(w[2], 4) + int_2_bin(w[1], 8)
             elif len(w) == 2:
+                args = "imm11=" + onlyInts(w[1])
                 binary = "11100" + int_2_bin(w[1], 11)  # words[1] ?
 
         case _:
-            sys.stdout.write("Nothing ")
-    return binary
+            sys.stdout.write("Error")
+    return bin_2_hex(binary), args
 
 
 def table_width(tab):
-    count = 0
+    nb = 0
     for char in tab.get_string():
         if char != "\n":
-            count += 1
+            nb += 1
         else:
             break
-    return count
+    return nb
 
 
 if __name__ == '__main__':
@@ -189,7 +229,7 @@ if __name__ == '__main__':
             exit(0)
 
         # Reads from given File
-        text, title = os.path.split(file_path)
+        t, title = os.path.split(file_path)
         for line in file.readlines():
             line = line.replace("\n", "").replace(",", "").lower()
             if line[-1:] == " ":
@@ -203,8 +243,7 @@ if __name__ == '__main__':
                 # Setting Text for Table
                 pc = bin_2_hex(int_2_bin(pc_count))
                 instruction = line
-                arguments = "None"
-                hex_op = bin_2_hex(parse_from_line(words))
+                hex_op, arguments = parse_from_line(words)
 
                 # Adding Binary string to Instructions
                 if hex_op:
@@ -251,4 +290,4 @@ if __name__ == '__main__':
             file.write("\n")
     file.close()
 
-    webbrowser.open(new_file_name)
+    # webbrowser.open(new_file_name)
