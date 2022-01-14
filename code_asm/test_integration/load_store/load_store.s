19a43d701657319425d73f5a164522d4fa0ceb5c
Matis Herrmann <matis_herrmann@hotmail.fr>
2022-01-14 09:00:13 +0100

    Nearly finished

--- code_asm/test_integration/load_store/load_store.s
+++ code_asm/test_integration/load_store/load_store.s
@@ -5,7 +5,7 @@ str r0, [sp, #4]
 str r1, [sp, #0]
 sub sp, #4
 ldr r2,[sp, #4]
-@r0 and r1 values should be 170, AA, 
+@r0, r1 values should be 170, AA,
 @r2 value shoul be 255, FF
 @In ROM, 04 value should be 255, FF, 
 @In ROM, 05 value should be 170, AA, 
